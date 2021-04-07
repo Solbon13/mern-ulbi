5f5acfb9f873ec1db090c5fcf26f4d71a7ccc67e
Solbon13 <solbon13@yandex.ru>
2021-04-07 17:12:19 +0800

    lesson 7 registration client

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      erwtre
-    </div>
-  );
-}
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import { store } from './reducer';
 import { Provider } from 'react-redux'
 
