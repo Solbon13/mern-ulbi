584ad055bef49149a4b138cc4aae8a4bca7510c5
Solbon13 <solbon13@yandex.ru>
2021-04-07 16:17:23 +0800

    lesson 6 redux

---
+++ client/src/reducer/fileReducer.js
@@ -0,0 +1,10 @@
+const defaulState = {
+
+}
+
+export default function fileReducer (state = defaulState, action) {
+    switch (action.type){
+        default:
+            return state
+    }
+}
---
+++ client/src/reducer/userReducer.js
@@ -0,0 +1,11 @@
+const defaulState = {
+    currentUser: {},
+    isAuth: false
+}
+
+export default function userReducer (state = defaulState, action) {
+    switch (action.type){
+        default:
+            return state
+    }
+}
