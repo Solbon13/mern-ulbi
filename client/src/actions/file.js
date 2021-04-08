40a03bfe6f757768f8c3dabd2a34d69aee086736
Solbon13 <solbon13@yandex.ru>
2021-04-08 10:48:31 +0800

    lesson 14 upload file client

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -1,13 +1,12 @@
-  
 import axios from 'axios'
 import {addFile, setFiles} from "../reducer/fileReducer";
 
 export function getFiles(dirId) {
     return async dispatch => {
         try {
-            const response = await axios.get(`http://localhost:5100/api/files${dirId ? '?parent='+dirId : ''}`, 
-            {headers:{Authorization:`Bearer ${localStorage.getItem('token')}`}}
-            )
+            const response = await axios.get(`http://localhost:5100/api/files${dirId ? '?parent='+dirId : ''}`, {
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+            })
             dispatch(setFiles(response.data))
         } catch (e) {
             alert(e.response.data.message)
@@ -25,6 +24,32 @@ export function createDir(dirId, name) {
             }, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
             })
+            dispatch(addFile(response.data))
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+    }
+}
+
+export function uploadFile(file, dirId) {
+    return async dispatch => {
+        try {
+            const formData = new FormData()
+            formData.append('file', file)
+            if (dirId) {
+                formData.append('parent', dirId)
+            }
+            const response = await axios.post(`http://localhost:5100/api/files/upload`, formData, {
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
+                onUploadProgress: progressEvent => {
+                    const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length');
+                    console.log('total', totalLength)
+                    if (totalLength) {
+                        let progress = Math.round((progressEvent.loaded * 100) / totalLength)
+                        console.log(progress)
+                    }
+                }
+            });
             dispatch(addFile(response.data))
         } catch (e) {
             alert(e.response.data.message)
--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,14 +1,16 @@
+  
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {createDir, getFiles} from "../../actions/file";
+import {createDir, getFiles, uploadFile} from "../../actions/file";
 import FileList from "./fileList/FileList";
 import './disk.css'
 import Popup from "./Popup";
-import {setPopupDisplay} from "../../reducer/fileReducer";
+import {setCurrentDir, setPopupDisplay} from "../../reducer/fileReducer";
 
 const Disk = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
+    const dirStack = useSelector(state => state.files.dirStack)
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
@@ -18,11 +20,25 @@ const Disk = () => {
         dispatch(setPopupDisplay('flex'))
     }
 
+    function backClickHandler() {
+        const backDirId = dirStack.pop()
+        dispatch(setCurrentDir(backDirId))
+    }
+
+    function fileUploadHandler(event) {
+        const files = [...event.target.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+    }
+
     return (
         <div className="disk">
             <div className="disk__btns">
-                <button className="disk__back">Назад</button>
+                <button className="disk__back" onClick={() => backClickHandler()}>Назад</button>
                 <button className="disk__create" onClick={() => showPopupHandler()}>Создать папку</button>
+                <div className="disk__upload">
+                    <label htmlFor="disk__upload-input" className="disk__upload-label">Загрузить файл</label>
+                    <input multiple={true} onChange={(event)=> fileUploadHandler(event)} type="file" id="disk__upload-input" className="disk__upload-input"/>
+                </div>
             </div>
             <FileList/>
             <Popup/>
