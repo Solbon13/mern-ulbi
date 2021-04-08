a01c3de7fc7876512577f4dd819bff2aba6ca189
Solbon13 <solbon13@yandex.ru>
2021-04-08 08:14:12 +0800

    lesson 8 auth client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.less'
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Registration from "./registration/Registration";
+import Registration from "./authorization/Registration";
+import Login from './authorization/Login';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
               <div className="wrap">
                   <Switch>
                       <Route path="/registration" component={Registration}/>
+                      <Route path="/login" component={Login}/>
                   </Switch>
               </div>
           </div>
